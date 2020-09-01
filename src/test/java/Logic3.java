02f2d8c79644c2db5461cf77d861890380ebf063
Lera1988 <lera.segal2@gmail.com>
2020-09-01 11:27:15 +0300

    fix Again value

--- src/test/java/Logic3.java
+++ src/test/java/Logic3.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Logic3 {
-    public static String Again;
+    public static String Again="y";
 
     static void myLogic3() {
             System.out.println("Invalid input, please use only Y/N");
